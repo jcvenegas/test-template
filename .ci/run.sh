0b13356475d78ceda670a82b464ac87d43d51cbd
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2020-04-14 06:06:29 +0000

    initial commit
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

---
+++ .ci/run.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 20XX Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+script_name=${0##*/}
+script_dir=$(dirname "$(readlink -f "$0")")
+
+die() {
+	local msg="$*"
+	echo "ERROR: $msg" >&2
+	exit 1
+}
+
+warn() {
+	local msg="$*"
+	echo "WARNING: $msg" >&2
+}
+
+info() {
+	local msg="$*"
+	echo "INFO: $msg" >&2
+}
+
+handle_error() {
+	local exit_code="${?}"
+	local line_number="${1:-}"
+	echo "Failed at ${script_name} +$line_number: ${BASH_COMMAND}"
+	exit "${exit_code}"
+}
+
+trap 'handle_error $LINENO' ERR
+
+command -v cargo || source $HOME/.cargo/env
+
+if [ "${CI:-}" != "true" ]; then
+	info "Running cargo-audit"
+	cargo audit
+
+	info "Running cargo-deny"
+	cargo deny check
+
+	info "Running cargo-outdated"
+	cargo outdated --exit-code 1
+
+	info "Running cargo-readme"
+	cargo readme >README.md
+
+	info "Running cargo-tree"
+	cargo tree
+fi
+
+info "Running clippy"
+cargo clippy --all-targets --all-features -- -D warnings
+cargo clippy --all -- -D warnings
+
+info "Running cargo fmt"
+cargo fmt -- --check
+
+info "Running tests"
+cargo test
---
+++ .ci/setup.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020 Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+script_name=${0##*/}
+script_dir=$(dirname "$(readlink -f "$0")")
+
+die() {
+	local msg="$*"
+	echo "ERROR: $msg" >&2
+	exit 1
+}
+
+warn() {
+	local msg="$*"
+	echo "WARNING: $msg" >&2
+}
+
+info() {
+	local msg="$*"
+	echo "INFO: $msg" >&2
+}
+
+OK() {
+	local msg="$*"
+	echo "[  OK  ] $msg" >&2
+}
+
+handle_error() {
+	local exit_code="${?}"
+	local line_number="${1:-}"
+	echo "Failed at ${script_name} +$line_number: ${BASH_COMMAND}"
+	exit "${exit_code}"
+}
+
+cargo_install() {
+	local -r program=${1}
+	info "Install ${program}"
+	command -v "${program}" || cargo install "${program}"
+	OK "${program} installed"
+}
+
+trap 'handle_error $LINENO' ERR
+
+source /etc/os-release
+
+info "Install distro build deps"
+"${script_dir}/setup-${ID}.sh"
+
+info "Installing rust"
+command -v cargo || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && source "${HOME}/.cargo/env"
+OK "rust installed"
+
+info "Install clippy"
+command -v clippy || rustup component add clippy
+OK "clippy installed"
+
+if [ "${CI:-}" != "true" ]; then
+	cargo_install cargo-audit
+	cargo_install cargo-deny
+	cargo_install cargo-outdated
+	cargo_install cargo-readme
+	cargo_install cargo-tree
+	cargo_install cargo-udeps
+fi
